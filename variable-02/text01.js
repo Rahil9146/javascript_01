7b3952041e7b2d7b963831fab55048c775832924
Rahil9146 <154125058+Rahil9146@users.noreply.github.com>
2024-12-24 10:08:21 +0000

    datatypes and vaiable

---
+++ variable-02/text01.js
@@ -0,0 +1,16 @@
+let name = "33"
+// var college =" AEC"
+// const Number =9146254661
+
+// city="chikhli"
+// console.table([name,college,Number,city]);
+let con1=Number(name);
+// console.log(con1);
+// console.log(typeof name);
+
+let isloggedIn=11;
+let isloggedIn1=String(isloggedIn)
+console.log(isloggedIn1);
+console.log(typeof isloggedIn1);
+
+
